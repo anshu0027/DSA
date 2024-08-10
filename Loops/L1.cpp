e65c5d5a45d157c44cc839b090163d975a5b2a1b
anshu0027 <you@example.com>
2024-08-10 13:24:08 +0530

    L1 updated

--- Loops/L1.cpp
+++ Loops/L1.cpp
@@ -1,11 +1,14 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main(){
     int i,j;
     
-    for(i=0;i<5;i++){
-        for(j=0;j<5;j++){
+    int n;
+    cin>>n;
+
+    for(i=0; i<n; i++){
+        for(j=0; j<n; j++){
             cout<< "*";
         }
         cout<<endl;
